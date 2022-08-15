83f4d1208bc5770b3fed5e89674c2bf3c31d278f
Ida Hägerth <92432753+idahagerth@users.noreply.github.com>
2022-08-15 20:13:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-
+const message = process.env.MESSAGE
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(message);
 });
 
 app.listen(PORT, () => {
